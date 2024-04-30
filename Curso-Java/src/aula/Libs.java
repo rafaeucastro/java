57ae30329b8b9f7168c10e7ea1903fd2184c6c7b
Rafael Castro <rafael.scastro@dataprev.gov.br>
2024-04-30 10:54:05 -0300

    Adicionar libs

---
+++ Curso-Java/src/aula/Libs.java
@@ -0,0 +1,9 @@
+package aula;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Libs {
+    public static void main(String[] args) {
+	System.out.println(StringUtils.lowerCase("HOBO"));
+    }
+}
